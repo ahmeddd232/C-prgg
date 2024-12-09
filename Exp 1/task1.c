9c4046d781c9a2bbce165d169156317ec80d772e
ahmeddd232 <ahmedraza00@eng.rizvi.edu.in>
2024-12-09 22:33:53 +0530

    Create task1.c

---
+++ Exp 1/task1.c
@@ -0,0 +1,31 @@
+/*
+    Expt. No : 1 , (task 1)
+    Title : WAP to print your name, age, class, division and UIN on the screen.
+     Use escape sequences to properly format the output.
+
+    Name: Ahmed Farooqui
+    Class & Div: ECS-E
+    UIN: 241S014
+ */
+ #include <stdio.h>
+
+int main() {
+    //printing details using function printf and using escape sequence  to organise the output in decent format 
+    printf("Full Name\t: Shaikh Kamran Qamaralam\n");
+    printf("Age\t\t: 19\n");
+    printf("Class\t\t: FE-ECs\n");
+    printf("Division\t: E\n");
+    printf("UIN\t\t: 2415012\n");
+    
+    return 0;
+}
+/* 
+OUTPUT
+
+Full Name	: Ahmed Farooqui
+Age			: 19
+Class		: FE-ECs
+Division	: E
+UIN			: 2415044
+
+*/
